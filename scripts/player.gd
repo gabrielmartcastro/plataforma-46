1a3d24203bcd8d14e5b9f86ddc6df178cf6df6f4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-20 09:11:42 -0300

    animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -300.0
-
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,5 +22,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+			# Altera a animação
+	if is_on_floor():	
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("walk")
+	else:
+		animated_sprite_2d.play("jump")
 
 	move_and_slide()
